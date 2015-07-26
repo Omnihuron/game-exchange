5904ab11c1197846c10678164a56a7ea3440770d
amclelland <allan.mclelland@gmail.com>
2015-07-26 09:01:56 +0000

    Added Game section of Admin Dashboard and list current games, and form for new game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :items
+  
+  validates :title,  presence: true, length: {maximum:20, minimum: 3 }
+  validates :slug,  presence: true, length: {maximum:50 }
 end
