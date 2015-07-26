2f6344ffa11a10fbfb8977aa1a9934464a43fd12
amclelland <allan.mclelland@gmail.com>
2015-07-26 02:46:38 +0000

    Added login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    if params['session']
+      create
+    end
   end
   
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        if params[:session][:remember_me] == '1'
-          remember user
-        else
-          forget user
-        end
-        redirect_back_or user
+
+      log_in user
+      flash[:default] = "Welcome back, #{user.name}!"
+      if params[:session][:remember_me] == '1'
+        remember user
       else
-        
-        message = "Account not activated. "
-        message += "Check your email for the activation link"
-        
-        flash[:warning] = message
-        redirect_to root_url
-        
+        forget user
       end
+      redirect_back_or home_path
+     
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:alert] = 'Invalid email/password combination'
       render 'new'
     end
   end
